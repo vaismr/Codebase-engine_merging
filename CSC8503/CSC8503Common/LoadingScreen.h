477fe5662727b8e87898f2b41a8a4125338d151c
Bailey <baileygardner25@gmail.com>
2020-02-27 21:36:02 +0000

    further code removal

--- CSC8503/CSC8503Common/LoadingScreen.h
+++ CSC8503/CSC8503Common/LoadingScreen.h
@@ -19,7 +19,6 @@ namespace NCL {
 		public:
 			LoadingScreen() {
 				loadingThread = std::thread([this] {this->Loading(); });
-
 				isLoading = true;
 			}
 
@@ -27,28 +26,25 @@ namespace NCL {
 				isLoading = false;
 				loadingThread.join();
 
-				delete renderer;
 				delete world;
-				renderer = nullptr;
 				world = nullptr;
 			}
 
 			void Loading() {
 				// since it's a new thread, we need a new renderer
-				world = new GameWorld();
+				world = new GameWorld(); 
 				renderer = new GameTechRenderer(*world);
-				StartLoadingScreen();
+				world->GetMainCamera()->SetCameraType(CameraType::Orthographic);
+				glClearColor(0, 0, 0, 0);
+
 				while (isLoading) {
 					UpdateLoadingScreen();
 					Sleep(600);
 				}
-				EndLoadingScreen();
-			}
 
-			void StartLoadingScreen() {
-				this->renderer = renderer;
-				world->GetMainCamera()->SetCameraType(CameraType::Orthographic);
-				glClearColor(0, 0, 0, 0);
+				world->GetMainCamera()->SetCameraType(CameraType::Perspective);
+				delete renderer;
+				renderer = nullptr;
 			}
 
 			void UpdateLoadingScreen() {
@@ -68,17 +64,12 @@ namespace NCL {
 				renderer->Render();
 			}
 
-			void EndLoadingScreen() {
-				world->GetMainCamera()->SetCameraType(CameraType::Perspective);
-				renderer = nullptr;
-			}
-
 		private:
 			int loadCount = 0;
+			bool isLoading;
+
 			Vector2 textPos = Vector2(50, 50);
 			string loadingText = "Loading.";
-
-			bool isLoading;
 
 			std::thread loadingThread;
 
