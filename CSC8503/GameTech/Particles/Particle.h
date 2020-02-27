0040fc88efa2a06eb47d3a677f5593badddab41a
Max Louis Dyson <max@thedysongang.com>
2020-02-27 15:52:31 +0000

    added struct to store data about a particle

---
+++ CSC8503/GameTech/Particles/Particle.h
@@ -0,0 +1,36 @@
+#pragma once
+#pragma once
+#include "../../CSC8503Common/PhysicsSystem.h"
+
+
+namespace NCL 
+{
+	namespace CSC8503 
+	{
+		struct Particle
+		{
+		public:
+			Particle() : position(0,0,0), velocity(0,0,0), colour(0,0,0,1), rotate(0), size(0), age(0), lifeTime(0)
+			{
+
+			}
+		private:
+			Vector3 position;
+			Vector3 velocity;
+			Vector4 colour;
+
+			float rotate;
+			float size;
+			float age;
+			float lifeTime;
+		};
+
+		typedef std::vector<Particle> ParticleBuffer;
+	}
+}
+
+
+
+
+
+
